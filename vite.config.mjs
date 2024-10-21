4ec933c451a17a99cc4d2dab81ba59bd1c716cf7
nin0dev <personal@nin0.dev>
2024-10-20 23:28:29 -0400

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,44 +1,13 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import path from 'path'
-import fs from 'fs'
-
-function getHtmlEntryFiles(srcDir) {
-	const entry = {};
-
-	function traverseDir(currentDir) {
-	  const files = fs.readdirSync(currentDir);
-
-	  files.forEach((file) => {
-		const filePath = path.join(currentDir, file);
-		const isDirectory = fs.statSync(filePath).isDirectory();
-
-		if (isDirectory) {
-		  // If it's a directory, recursively traverse it
-		  traverseDir(filePath);
-		} else if (path.extname(file) === '.html') {
-		  // If it's an HTML file, add it to the entry object
-		  const name = path.relative(srcDir, filePath).replace(/\..*$/, '');
-		  entry[name] = filePath;
-		}
-	  });
-	}
-
-	traverseDir(srcDir);
-
-	return entry;
-}
-
 
 export default defineConfig({
-	root: 'src',
-	build: {
-		rollupOptions: {
-			input: getHtmlEntryFiles('src')
-		},
-		outDir: '../dist',
-		emptyOutDir: true
-	},
-	optimizeDeps: {
-		entries: 'src/**/*{.html,.css,.js}'
-	}
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        "login.html": resolve(__dirname, 'login.html'),
+      },
+    },
+  },
 })
